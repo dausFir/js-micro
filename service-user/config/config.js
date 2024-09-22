978e61ddd73acf04503c96995bfd01239406ac72
dausFir <a.firdausop@gmail.com>
2024-09-22 20:47:33 +0700

    add base file

---
+++ service-user/config/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const {
+    HOSTNAME,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USERNAME
+} = process.env
+
+module.exports = {
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": HOSTNAME,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": HOSTNAME,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": HOSTNAME,
+    "dialect": "mysql"
+  }
+}
